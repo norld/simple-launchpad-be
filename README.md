9b3fe2ee42adcd8e5bc68b037c92d89a49c22090
Made Supardi <grymore@gmail.com>
2022-11-26 13:49:22 +0800

    [MVP-1] initialize code

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# simple-launchpad-be
+# 🚀 Getting started with Launchpad Backend
+
+## `package`
+
+```
+pnpm i
+# or
+npm i
+```
+
+### `develop`
+
+```
+npm run develop
+# or
+yarn develop
+```
+
+### `start`
+
+```
+npm run start
+# or
+yarn start
+```
+
+### `build`
+
+```
+npm run build
+# or
+yarn build
+```
---
+++ config/server.js
@@ -0,0 +1,19 @@
+/* eslint-disable quotes */
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  production: false,
+  proxy: {
+    enabled: false,
+  },
+  cron: {
+    enabled: false,
+  },
+  admin: {
+    autoOpen: true,
+  },
+  url: "https://app.grymore.xyz",
+});
