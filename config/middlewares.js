1e7091a6a7296a5fda6c37686c2bb69a2f6077a7
Made Supardi <grymore@gmail.com>
2022-11-26 15:07:30 +0800

    [MVP-1] enable middleware cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,21 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
-  // {
-  //   name: "strapi::cors",
-  //   config: {
-  //     enabled: true,
-  //     headers: "*",
-  //     origin: [
-  //       "http://localhost:1337",
-  //       "http://localhost:3000",
-  //       "https://grymore.xyz",
-  //       "https://app.grymore.xyz",
-  //       "https://simple-launchpad-be.herokuapp.com",
-  //     ],
-  //   },
-  // },
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "http://localhost:1337",
+        "http://localhost:3000",
+        "https://grymore.xyz",
+        "https://app.grymore.xyz",
+        "https://simple-launchpad-be.herokuapp.com",
+        "https://simple-launchpad-fe-grymore.vercel.app",
+      ],
+    },
+  },
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
